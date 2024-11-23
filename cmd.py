8249d32e2b5cb4c843451e892ae70c0972b70784
Noface <166717111+noface1200@users.noreply.github.com>
2024-11-23 22:46:10 +0000

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,26 +1,40 @@
-import subprocess, random, colorama
-from requests import post
+import subprocess, random, urllib.parse, http.client
+import sys
 
 done = []
 
 file_object = open('valid.txt', 'w', buffering=1)
 
-colorama.init(autoreset=True)
+GREEN = "\033[32m"
+RED = "\033[31m"
+RESET = "\033[0m"
 
 def generate_ip():
     return '.'.join(str(random.randint(0, 255)) for _ in range(4))
 
+def ping_ip(ip):
+    request = subprocess.Popen(['ping', '-n', '1', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = request.communicate()
+    return 'Lost = 0' in out.decode()
+
+def sweep_ip(ip):
+    base_ip = '.'.join(ip.split('.')[:3]) + '.'
+    print(f'{GREEN}[Sweeping] {RESET}{ip}')
+    for i in range(1, 255):
+        test_ip = base_ip + str(i)
+        if ping_ip(test_ip):
+            print(f'{GREEN}[Access Granted] {RESET}{test_ip}')
+            file_object.write(f'valid ip: {test_ip}\n')
+            file_object.flush()
+
 while True:
     ipv_gen = generate_ip()
-    request = subprocess.Popen(['ping', '-n', '1', ipv_gen], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    out, err = request.communicate()
-    
-    if 'Lost = 0' in out.decode():
+    if ping_ip(ipv_gen):
         if ipv_gen not in done:
-            print(f'{colorama.Fore.GREEN}[Access Granted] {colorama.Fore.RESET}{ipv_gen}')
+            print(f'{GREEN}[Access Granted] {RESET}{ipv_gen}')
             file_object.write(f'valid ip: {ipv_gen}\n')
             file_object.flush()
             done.append(ipv_gen)
+            sweep_ip(ipv_gen)
     else:
-        print(f'{colorama.Fore.RED}[Access Denied] {colorama.Fore.RESET}{ipv_gen}')
+        print(f'{RED}[Access Denied] {RESET}{ipv_gen}')
